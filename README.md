c9f1b79b36a209ee232a53d77765fa1ca82e9416
Noel Tibbles <noeltibbles@Noels-Laptop.local>
2024-12-19 07:40:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Resize Text by Equa11y
+Quickly and accurately resizes the text on a website. This extension accurately emulates the native functionality of the browser to only resize text.
+
+## Loading an unpacked extension
+To load an unpacked extension in developer mode:
+
+1. Go to the Extensions page by entering chrome://extensions in a new tab. (By design chrome:// URLs are not linkable.)
+    - Alternatively, click the Extensions menu puzzle button and select Manage Extensions at the bottom of the menu.
+    - Or, click the Chrome menu, hover over More Tools, then select Extensions.
+2. Enable Developer Mode by clicking the toggle switch next to Developer mode.
+3. Click the Load unpacked button and select the extension directory.
+
+Voila! 
---
+++ popup.js
@@ -0,0 +1,51 @@
+import { toggleZoom } from "./resize-text.js";
+
+let textPort = 0;
+let value = {};
+
+document.addEventListener('DOMContentLoaded', init);
+chrome.runtime.onConnect.addListener(function(port) {
+    textPort = port;
+});
+
+function getTabId() {
+    return new Promise(resolve => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            resolve(tabs[0].id);
+        });
+    })
+}
+
+async function init() {
+    document.getElementById("textZoom").addEventListener('input', zoomChange);
+    value = document.getElementById("zoomValue");
+    loadScript();
+    restoreSlider();
+}
+
+async function loadScript() {
+    const id = await getTabId();
+    chrome.scripting.executeScript({
+        target: { tabId: id },
+        function: toggleZoom
+    });
+}
+
+function restoreSlider() {
+    chrome.storage.sync.get().then(result => {
+        const slider = document.getElementById('textZoom');
+        const zoomValue = document.getElementById('zoomValue');
+        const value = result['resizeTextSlider']?.slider
+        slider.value = value || 1;
+        zoomValue.textContent = `${Math.round(value * 100)}%`;
+    });
+}
+
+function zoomChange(event) {
+    let store = {};
+    let percent = Number(event.target.value);
+    value.textContent = `${Math.round(percent * 100)}%`;
+    textPort.postMessage({zoomLevel: percent});
+    store['resizeTextSlider'] = { slider: percent };
+    chrome.storage.sync.set( store );
+}
